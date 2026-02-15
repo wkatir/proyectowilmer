d2954bf66bdd213a8a067d68df7ec57c2c8ca444
Wilmer Henrry Salazar Martinez <wilmerhenrysalazarmartinez@gmail.com>
2026-02-15 14:07:38 -0600

    feat: remove Student.tsx component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,5 @@
 import { Content1 } from '@/components/Content'
 import { Footer2 } from '@/components/Footer'
-import Student from '@/components/Student'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
@@ -10,7 +9,6 @@ export const Route = createFileRoute('/')({
 function Index() {
   return (
     <div>
-    <Student></Student>
     <Content1></Content1>
     <Footer2></Footer2>
     </div>
